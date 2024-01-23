d46f7e0c4f3e9edea18ec04a7fd4647fca696d43
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-23 23:17:17 +0545

    feat: index api route created

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def index(request):
-    return JsonResponse({'message': 'Hello, world!'})
+    return Response({'message': 'Hello, world!'})
