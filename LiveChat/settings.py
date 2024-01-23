e163649f6f4bf8088df90d991074c9308432170c
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-23 22:59:52 +0545

    fix: Debug mode set to False

--- LiveChat/settings.py
+++ LiveChat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uaw-$c#j%v@at(n8bsjjv_^mqt))&^#s+wgb9sg@5%=ty&+gb6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
