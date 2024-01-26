2e826aee06fd40f8c03c8dd8fc984a5add0df6ec
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-26 23:27:22 +0545

    feat: bcrypt with user registration

--- api/models.py
+++ api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class User(models.Model):
     firstName = models.CharField(max_length=50)
     middleName = models.CharField(max_length=50, blank=True)
     lastName = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
