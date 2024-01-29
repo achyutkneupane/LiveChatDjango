2a573a3cc31b8a6c9424b3d1b9f0bf3e3c1ac80c
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-29 21:23:36 +0545

    refactor: Urls grouped

--- LiveChat/urls.py
+++ LiveChat/urls.py
@@ -19,5 +19,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('the_auth/', include('the_auth.urls')),
+
+    path('api/', include([
+        path('auth/', include('the_auth.urls'), name='auth'),
+    ]), name='api'),
 ]
--- the_auth/urls.py
+++ the_auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/register', views.register, name='auth.register'),
-    path('auth/login', views.login, name='auth.login')
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login')
 ]
