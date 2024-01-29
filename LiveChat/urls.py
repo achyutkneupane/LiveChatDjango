17cd4f0b9d3a03217ed2c92816d7dafa384ffe63
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-29 21:08:53 +0545

    docs: Swagger Setup done

--- LiveChat/urls.py
+++ LiveChat/urls.py
@@ -3,17 +3,16 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Live Chat API",
         default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        description="Live Chat API created for Applied Programming Project",
+        contact=openapi.Contact(email="achyutkneupane@gmail.com"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=[permissions.AllowAny]
 )
 
 urlpatterns = [
