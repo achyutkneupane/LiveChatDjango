8b31e15d482bf79205758abf0cc900d6192e28ee
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-30 00:55:01 +0545

    feat: Chatbox Model created

--- chatbox/models.py
+++ chatbox/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Chatbox(models.Model):
+    id = models.AutoField(primary_key=True)
+    participants = models.ManyToManyField('the_auth.User')
+    name = models.CharField(max_length=255, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "chatboxes"
+        ordering = ["-createdAt"]
+        verbose_name = "Chatbox"
+        verbose_name_plural = "Chatboxes"
+        app_label = "chatbox"
