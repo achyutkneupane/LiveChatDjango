e669993dac2d3291fd45f8b6b1b9703c8426fbde
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2024-01-31 02:43:27 +0545

    feat: Current user API endpoint added (#18)
    * feat: Current User API added
    
    * feat: localhost added as allowed host

--- the_auth/urls.py
+++ the_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TheAuthAPIView, TheAuthRegisterView, TheAuthLoginView
+from .views import CurrentUserView, TheAuthRegisterView, TheAuthLoginView
 
 urlpatterns = [
     # path('', TheAuthAPIView.as_view({'get': 'index'}), name='auth'),
     path('register', TheAuthRegisterView.as_view({'post': 'register'}), name='register'),
-    path('login', TheAuthLoginView.as_view({'post': 'login'}), name='login')
+    path('login', TheAuthLoginView.as_view({'post': 'login'}), name='login'),
+    path('get-user', CurrentUserView.as_view({'get': 'get_user'}), name='get-user'),
 ]
